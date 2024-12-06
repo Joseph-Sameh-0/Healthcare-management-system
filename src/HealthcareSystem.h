f378ce7092b8965e9b3075964ed171096753de29
Jox7 <113832133+Jox7@users.noreply.github.com>
2024-12-06 13:46:06 +0200

    Update HealthcareSystem.h

--- src/HealthcareSystem.h
+++ src/HealthcareSystem.h
@@ -15,6 +15,8 @@ private:
 public:
     void addDoctor(const string &doctorID, const string &name, const string &address);
     void addAppointment(const string &appointmentID, const string &date, const string &doctorID);
+    void deleteDoctor (const string &doctorID);
+    void deleteAppointment (const string &AppointmentID);
     // Other methods: deleteDoctor, deleteAppointment, etc.
     PrimaryIndex dIndex;
     PrimaryIndex aIndex;
